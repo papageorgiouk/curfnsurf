b21681feacbff417d87360efb6ff246bbdc7ca8f
Konstantinos Papageorgiou <kotsios.p@gmail.com>
2020-03-28 15:42:06 +0200

    Create viewpager navigation listener

--- app/src/main/java/com/papageorgiouk/curfnsurf/ui/viewpager.kt
+++ app/src/main/java/com/papageorgiouk/curfnsurf/ui/viewpager.kt
@@ -3,6 +3,9 @@ package com.papageorgiouk.curfnsurf.ui
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.papageorgiouk.curfnsurf.ui.form.id.IdFragment
@@ -29,4 +32,38 @@ fun ViewPager2.hasBackStack() = this.currentItem > 0
 fun ViewPager2.popBackStack() {
     this.currentItem = this.currentItem - 1
 }
+
+fun ViewPager2.directionalNavigationListener(lifecycleOwner: LifecycleOwner, direction: (Direction, Int) -> Unit) {
+
+    val pageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
+
+        private var previousSelected: Int = -1
+
+        override fun onPageSelected(position: Int) {
+            val isBack = position < previousSelected
+            val isForward = position > previousSelected
+
+            previousSelected = position
+
+            if (isBack) direction(Direction.BACK, position)
+            else if (isForward)  direction(Direction.FORWARD, position)
+        }
+    }
+
+    lifecycleOwner.lifecycle.addObserver(object : LifecycleObserver {
+        @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+        fun onCreated(source: LifecycleOwner?) {
+            this@directionalNavigationListener.registerOnPageChangeCallback(pageChangeCallback)
+
+        }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+        fun onDestroy() {
+            this@directionalNavigationListener.unregisterOnPageChangeCallback(pageChangeCallback)
+        }
+    })
+
+}
+
+enum class Direction { BACK, FORWARD }
 
