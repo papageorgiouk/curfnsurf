85560c081d481e1ecd868d7df5980ce0510d8de4
Konstantinos Papageorgiou <kotsios.p@gmail.com>
2020-03-30 01:21:02 +0300

    Clean up layouts, make adjustResize-friendly

--- app/src/main/java/com/papageorgiouk/curfnsurf/MainActivity.kt
+++ app/src/main/java/com/papageorgiouk/curfnsurf/MainActivity.kt
@@ -39,9 +39,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
 
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
-
         pager.apply {
             adapter = pagerAdapter
             directionalNavigationListener(this@MainActivity) { direction, position ->
